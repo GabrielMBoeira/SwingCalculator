4895ae9785bcb5a07fe5b97f1715acebe462fd84
GabrielMBoeira <gboeira777@gmail.com>
2020-01-26 15:54:55 -0300

    CalculadoraSwing Java

---
+++ CalculadoraSwing/src/telacalc/Metodos.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package telacalc;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Metodos {
+    
+    public Metodos(){
+
+}
+    public Double somarValoresCalc(Double numero1, Double numero2){
+        Double resultado = numero1 + numero2;
+        return resultado;
+    }
+    public Double subtrairValoresCalc(Double numero1, Double numero2){
+        Double resultado = numero1 - numero2;
+        return resultado;
+    }
+    public Double dividirValoresCalc(Double numero1, Double numero2){
+        Double resultado = numero1 / numero2;
+        return resultado;
+    }
+    public Double multiplicarValoresCalc(Double numero1, Double numero2){
+        Double resultado = numero1 * numero2;
+        return resultado;
+    }
+ 
+    
+}
+
+
